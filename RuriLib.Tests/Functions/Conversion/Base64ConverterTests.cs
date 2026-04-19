f17763144721d3e19d97a3188b9278a6d6e9fcfe
Ruri <ruridev@protonmail.com>
2026-04-19 21:46:51 +0200

    Refactor more tests

--- RuriLib.Tests/Functions/Conversion/Base64ConverterTests.cs
+++ RuriLib.Tests/Functions/Conversion/Base64ConverterTests.cs
@@ -1,16 +1,15 @@
-﻿using RuriLib.Functions.Conversion;
 using System.Text;
+using RuriLib.Functions.Conversion;
 using Xunit;
 
-namespace RuriLib.Tests.Functions.Conversion
+namespace RuriLib.Tests.Functions.Conversion;
+
+public class Base64ConverterTests
 {
-    public class Base64ConverterTests
+    [Fact]
+    public void ToByteArray_DottedString_RemoveDotsAndDecode()
     {
-        [Fact]
-        public void ToByteArray_DottedString_RemoveDotsAndDecode()
-        {
-            byte[] bytes = Base64Converter.ToByteArray("aGVsbG8gaG93I.GFyZSB5b3U/");
-            Assert.Equal("hello how are you?", Encoding.UTF8.GetString(bytes));
-        }
+        var bytes = Base64Converter.ToByteArray("aGVsbG8gaG93I.GFyZSB5b3U/");
+        Assert.Equal("hello how are you?", Encoding.UTF8.GetString(bytes));
     }
 }

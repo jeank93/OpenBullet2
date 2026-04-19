631e11616d1d3875bc08f5dbe7091ebc9e618ccd
Ruri <ruridev@protonmail.com>
2026-04-19 21:18:57 +0200

    Format parser tests

--- RuriLib.Tests/Functions/Parsing/JsonParserTests.cs
+++ RuriLib.Tests/Functions/Parsing/JsonParserTests.cs
@@ -1,33 +1,32 @@
-﻿using RuriLib.Functions.Parsing;
 using System.Linq;
+using RuriLib.Functions.Parsing;
 using Xunit;
 
-namespace RuriLib.Tests.Functions.Parsing
+namespace RuriLib.Tests.Functions.Parsing;
+
+public class JsonParserTests
 {
-    public class JsonParserTests
+    private readonly string jsonObject = "{ \"key\": \"value\" }";
+    private readonly string jsonArray = "[ \"elem1\", \"elem2\" ]";
+
+    [Fact]
+    public void GetValuesByKey_GetStringFromObject_ReturnValue()
     {
-        private readonly string jsonObject = "{ \"key\": \"value\" }";
-        private readonly string jsonArray = "[ \"elem1\", \"elem2\" ]";
+        var match = JsonParser.GetValuesByKey(jsonObject, "key").FirstOrDefault();
+        Assert.Equal("value", match);
+    }
 
-        [Fact]
-        public void GetValuesByKey_GetStringFromObject_ReturnValue()
-        {
-            var match = JsonParser.GetValuesByKey(jsonObject, "key").FirstOrDefault();
-            Assert.Equal("value", match);
-        }
+    [Fact]
+    public void GetValuesByKey_MissingKey_MatchNothing()
+    {
+        var match = JsonParser.GetValuesByKey(jsonObject, "dummy").FirstOrDefault();
+        Assert.Null(match);
+    }
 
-        [Fact]
-        public void GetValuesByKey_MissingKey_MatchNothing()
-        {
-            var match = JsonParser.GetValuesByKey(jsonObject, "dummy").FirstOrDefault();
-            Assert.Null(match);
-        }
-
-        [Fact]
-        public void GetValuesByKey_GetStringsFromArray_ReturnValues()
-        {
-            var match = JsonParser.GetValuesByKey(jsonArray, "[*]").ToArray();
-            Assert.Equal(new string[] { "elem1", "elem2" }, match);
-        }
+    [Fact]
+    public void GetValuesByKey_GetStringsFromArray_ReturnValues()
+    {
+        var match = JsonParser.GetValuesByKey(jsonArray, "[*]").ToArray();
+        Assert.Equal(new[] { "elem1", "elem2" }, match);
     }
 }

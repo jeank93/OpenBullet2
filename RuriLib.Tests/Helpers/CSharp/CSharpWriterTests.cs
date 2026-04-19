0175d42ac994c38afc06aa0152c849c2a914f0da
Ruri <ruridev@protonmail.com>
2026-04-19 11:34:02 +0200

    Refactor CSharpWriter and tests

--- RuriLib.Tests/Helpers/CSharp/CSharpWriterTests.cs
+++ RuriLib.Tests/Helpers/CSharp/CSharpWriterTests.cs
@@ -1,26 +1,49 @@
-﻿using RuriLib.Helpers.CSharp;
+using RuriLib.Helpers.CSharp;
 using Xunit;
 
-namespace RuriLib.Tests.Helpers.CSharp
+namespace RuriLib.Tests.Helpers.CSharp;
+
+public class CSharpWriterTests
 {
-    public class CSharpWriterTests
+    [Fact]
+    public void SerializeInterpString_Alone_ReplaceCorrectly()
     {
-        [Fact]
-        public void SerializeInterpString_Alone_ReplaceCorrectly()
-        {
-            Assert.Equal("$\"{value}\"", CSharpWriter.SerializeInterpString("<value>"));
-        }
+        Assert.Equal("$\"{value}\"", CSharpWriter.SerializeInterpString("<value>"));
+    }
 
-        [Fact]
-        public void SerializeInterpString_Surrounded_ReplaceCorrectly()
-        {
-            Assert.Equal("$\"my {value} is cool\"", CSharpWriter.SerializeInterpString("my <value> is cool"));
-        }
+    [Fact]
+    public void SerializeInterpString_Surrounded_ReplaceCorrectly()
+    {
+        Assert.Equal("$\"my {value} is cool\"", CSharpWriter.SerializeInterpString("my <value> is cool"));
+    }
 
-        [Fact]
-        public void SerializeInterpString_SingleCharacter_ReplaceCorrectly()
-        {
-            Assert.Equal("$\"my {a} is cool\"", CSharpWriter.SerializeInterpString("my <a> is cool"));
-        }
+    [Fact]
+    public void SerializeInterpString_SingleCharacter_ReplaceCorrectly()
+    {
+        Assert.Equal("$\"my {a} is cool\"", CSharpWriter.SerializeInterpString("my <a> is cool"));
+    }
+
+    [Fact]
+    public void SerializeByteArray_Null_ReturnsNullLiteral()
+    {
+        Assert.Equal("null", CSharpWriter.SerializeByteArray(null));
+    }
+
+    [Fact]
+    public void SerializeList_Null_ReturnsNullLiteral()
+    {
+        Assert.Equal("null", CSharpWriter.SerializeList(null));
+    }
+
+    [Fact]
+    public void SerializeDictionary_Null_ReturnsNullLiteral()
+    {
+        Assert.Equal("null", CSharpWriter.SerializeDictionary(null));
+    }
+
+    [Fact]
+    public void ToPrimitive_Null_ReturnsNullLiteral()
+    {
+        Assert.Equal("null", CSharpWriter.ToPrimitive(null));
     }
 }

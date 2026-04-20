bc8e84a0a1da8ff87f74fe916864df6a9e1aaf89
Ruri <ruridev@protonmail.com>
2026-04-20 10:03:29 +0200

    Refactor more files and add tests

--- RuriLib/Providers/Emails/IEmailDomainRepository.cs
+++ RuriLib/Providers/Emails/IEmailDomainRepository.cs
@@ -1,17 +1,16 @@
-﻿using RuriLib.Functions.Networking;
+using RuriLib.Functions.Networking;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace RuriLib.Providers.Emails
-{
-    public interface IEmailDomainRepository
-    {
-        Task<IEnumerable<HostEntry>> GetImapServers(string domain);
-        Task<IEnumerable<HostEntry>> GetPop3Servers(string domain);
-        Task<IEnumerable<HostEntry>> GetSmtpServers(string domain);
+namespace RuriLib.Providers.Emails;
 
-        Task TryAddImapServer(string domain, HostEntry server);
-        Task TryAddPop3Server(string domain, HostEntry server);
-        Task TryAddSmtpServer(string domain, HostEntry server);
-    }
+public interface IEmailDomainRepository
+{
+    Task<IEnumerable<HostEntry>> GetImapServers(string domain);
+    Task<IEnumerable<HostEntry>> GetPop3Servers(string domain);
+    Task<IEnumerable<HostEntry>> GetSmtpServers(string domain);
+
+    Task TryAddImapServer(string domain, HostEntry server);
+    Task TryAddPop3Server(string domain, HostEntry server);
+    Task TryAddSmtpServer(string domain, HostEntry server);
 }

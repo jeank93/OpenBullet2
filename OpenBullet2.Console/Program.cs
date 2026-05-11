5f61ece73808f8ad172392fbc8cd6cf802bf100b
Ruri <ruridev@protonmail.com>
2026-05-11 14:51:41 +0200

    Add single run debug mode to console project
    Closes #330

--- OpenBullet2.Console/Program.cs
+++ OpenBullet2.Console/Program.cs
@@ -1,21 +1,22 @@
 using CommandLine;
-using CommandLine.Text;
 using RuriLib.Helpers;
+using RuriLib.Logging;
+using RuriLib.Models.Bots;
+using RuriLib.Models.Data;
 using RuriLib.Models.Data.DataPools;
+using RuriLib.Models.Debugger;
 using RuriLib.Models.Hits;
 using RuriLib.Models.Hits.HitOutputs;
 using RuriLib.Models.Jobs;
-using RuriLib.Models.Proxies;
 using RuriLib.Models.Proxies.ProxySources;
-using RuriLib.Services;
 using RuriLib.Parallelization.Models;
+using RuriLib.Providers.RandomNumbers;
+using RuriLib.Services;
+using Spectre.Console;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Threading;
-using RuriLib.Models.Bots;
-using RuriLib.Models.Data;
 
 namespace OpenBullet2.Console;
 
@@ -29,111 +30,103 @@ internal class Program
      *                                                                                                                           *
      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    private class Options
-    {
-        [Option('c', "config", Required = true, HelpText = "Configuration file to be processed.")]
-        public string ConfigFile { get; set; } = string.Empty;
-
-        [Option('w', "wordlist", Required = false, HelpText = "Wordlist file to be processed.")]
-        public string? WordlistFile { get; set; }
-
-        [Option("wordlist-range", Required = false, HelpText = "Wordlist Range to be processed, syntax: start,amount,step,pad(True or False) . Only start and amount are necessary.")]
-        public string? WordlistRange { get; set; }
-
-        [Option("wltype", Required = true, HelpText = "Type of the wordlist loaded (see Environment.ini for all allowed types).")]
-        public string WordlistType { get; set; } = string.Empty;
-
-        [Option('p', "proxies", Default = null, HelpText = "Proxy file to be processed.")]
-        public string? ProxyFile { get; set; }
-
-        [Option("ptype", Default = ProxyType.Http, HelpText = "Type of proxies loaded (Http, Socks4, Socks5).")]
-        public ProxyType ProxyType { get; set; }
-
-        [Option("pmode", Default = JobProxyMode.Default, HelpText = "The proxy mode (On, Off, Default).")]
-        public JobProxyMode ProxyMode { get; set; }
-
-        [Option('s', "skip", Default = 1, HelpText = "Number of lines to skip in the Wordlist.")]
-        public int Skip { get; set; }
-
-        [Option('b', "bots", Default = 0, HelpText = "Number of concurrent bots working. If not specified, the config default will be used.")]
-        public int BotsNumber { get; set; }
-
-        [Option('v', "verbose", Default = false, HelpText = "Prints fails and task errors.")]
-        public bool Verbose { get; set; }
-
-        [Usage(ApplicationAlias = "dotnet OpenBullet2.Console.dll")]
-        public static IEnumerable<Example> Examples => new List<Example>() {
-                    new("Simple POC CLI Implementation of RuriLib that executes a MultiRunJob.",
-                        new Options {
-                            ConfigFile = "config.opk",
-                            WordlistFile = "rockyou.txt",
-                            WordlistType = "Default",
-                            ProxyFile = "proxies.txt",
-                            ProxyType = ProxyType.Http,
-                            ProxyMode = JobProxyMode.Default,
-                            Skip = 1,
-                            BotsNumber = 1,
-                            Verbose = false
-                        }
-                    )
-                };
-    }
-
     private static MultiRunJob? job;
-    private static Options? options;
-    private static bool completed = false;
+    private static ConsoleOptions? options;
+    private static bool completed;
 
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
         ThreadPool.SetMinThreads(1000, 1000);
 
         System.Console.Title = "OpenBullet 2 (Console POC)";
-        System.Console.WriteLine(@"
+        AnsiConsole.Write(new Text(@"
 This is a POC (Proof of Concept) implementation of RuriLib as a console application.
 The functionalities supported here don't even come close to the ones of the main implementation.
 Feel free to contribute to the versatility of this project by adding the missing functionalities and submitting a PR.
-");
+", Style.Plain));
 
-        // Parse the Options
-        Parser.Default.ParseArguments<Options>(args)
-          .WithParsed(opts => Run(opts))
-          .WithNotParsed((errs) => { });
+        var exitCode = 0;
+
+        CommandLine.Parser.Default.ParseArguments<ConsoleOptions>(args)
+            .WithParsed(opts => exitCode = Run(opts))
+            .WithNotParsed(_ => exitCode = 1);
+
+        return exitCode;
     }
 
-    private static void Run(Options opts)
+    private static int Run(ConsoleOptions opts)
     {
         options = opts;
+        completed = false;
+
+        var validationErrors = ConsoleRunPlanner.Validate(opts);
+        if (validationErrors.Count > 0)
+        {
+            foreach (var error in validationErrors)
+            {
+                WriteLine($"Argument error: {error}", "#ff6347");
+            }
+
+            WriteLine("Use --help to see the supported options.", "#ff6347");
+            return 1;
+        }
 
         var rlSettings = new RuriLibSettingsService("UserData");
         var pluginRepo = new PluginRepository("UserData/Plugins");
 
-        // Unpack the config
         using var fs = new FileStream(opts.ConfigFile, FileMode.Open);
         var config = ConfigPacker.UnpackAsync(fs).Result;
 
-        DataPool dataPool;
-        if (string.IsNullOrEmpty(opts.WordlistFile) && !string.IsNullOrEmpty(opts.WordlistRange))
+        var customInputsAnswers = AskCustomInputs(config);
+
+        if (ConsoleRunPlanner.GetRunMode(opts) == ConsoleRunMode.SingleRunDebug)
         {
-            var splitRange = opts.WordlistRange.Split(",");
-            dataPool = new RangeDataPool(
-                start: Convert.ToInt64(splitRange[0]),
-                amount: Convert.ToInt32(splitRange[1]),
-                step: splitRange.Length > 2 ? Convert.ToInt32(splitRange[2]) : default,
-                pad: splitRange.Length > 3 && Convert.ToBoolean(splitRange[3]),
-                opts.WordlistType
-                );
+            RunSingleDebugSession(config, rlSettings, pluginRepo, opts);
+            WaitForExit();
+            return 0;
         }
-        else
+
+        RunMultiRunJob(config, rlSettings, pluginRepo, opts, customInputsAnswers);
+        WaitForExit();
+        return 0;
+    }
+
+    private static void RunSingleDebugSession(RuriLib.Models.Configs.Config config,
+        RuriLibSettingsService rlSettings, PluginRepository pluginRepo, ConsoleOptions opts)
+    {
+        var debuggerOptions = ConsoleRunPlanner.BuildDebuggerOptions(opts);
+
+        using var debugger = new ConfigDebugger(config, debuggerOptions)
         {
-            if (string.IsNullOrEmpty(opts.WordlistFile))
+            RuriLibSettings = rlSettings,
+            PluginRepo = pluginRepo,
+            RNGProvider = new DefaultRNGProvider()
+        };
+
+        debugger.NewLogEntry += PrintDebuggerLog;
+        debugger.StatusChanged += (_, status) =>
+        {
+            if (status != ConfigDebuggerStatus.WaitingForStep)
             {
-                throw new ArgumentException("A wordlist file must be provided when no wordlist range is specified.");
+                return;
             }
 
-            dataPool = new FileDataPool(opts.WordlistFile, opts.WordlistType);
-        }
+            WriteStepPromptPanel(config);
+            System.Console.ReadLine();
+            debugger.TryTakeStep();
+        };
 
-        // Setup the job
+        debugger.Run().GetAwaiter().GetResult();
+
+        WriteLine("Single-run debug session completed.", "#7fffd4");
+    }
+
+    private static void RunMultiRunJob(RuriLib.Models.Configs.Config config,
+        RuriLibSettingsService rlSettings, PluginRepository pluginRepo, ConsoleOptions opts,
+        Dictionary<string, string> customInputsAnswers)
+    {
+        var dataPool = BuildDataPool(opts);
+
         job = new MultiRunJob(rlSettings, pluginRepo)
         {
             Config = config,
@@ -148,44 +141,147 @@ Feel free to contribute to the versatility of this project by adding the missing
             HitOutputs = [new FileSystemHitOutput("UserData/Hits")],
             BotLimit = opts.BotsNumber,
             Skip = opts.Skip,
+            CustomInputsAnswers = customInputsAnswers,
             CurrentBotDatas = new BotData[opts.BotsNumber]
         };
 
-        // Ask custom inputs (if any)
-        foreach (var input in config.Settings.InputSettings.CustomInputs)
-        {
-            System.Console.WriteLine($"{input.Description} ({input.DefaultAnswer}): ");
-            var answer = System.Console.ReadLine();
-            job.CustomInputsAnswers[input.VariableName] = string.IsNullOrWhiteSpace(answer)
-                ? input.DefaultAnswer
-                : answer;
-        }
-
-        // Hook event handlers
-        job.OnCompleted += (sender, args) => completed = true;
+        job.OnCompleted += (_, _) => completed = true;
         job.OnResult += PrintResult;
         job.OnTaskError += PrintTaskError;
-        job.OnError += (sender, ex) => System.Console.WriteLine($"Error: {ex.Message}", Color.Tomato);
+        job.OnError += (_, ex) => WriteLine($"Error: {ex.Message}", "#ff6347");
 
-        // Start the job
         job.Start().Wait();
 
-        // Wait until it finished
         while (!completed)
         {
             Thread.Sleep(100);
             UpdateTitle();
         }
 
-        // Print colored finish message
         System.Console.Write($"Finished. Found: ");
-        System.Console.Write($"{job.DataHits} hits, ", Color.GreenYellow);
-        System.Console.Write($"{job.DataCustom} custom, ", Color.DarkOrange);
-        System.Console.WriteLine($"{job.DataToCheck} to check.", Color.Aquamarine);
+        Write($"{job.DataHits} hits, ", "#adff2f");
+        Write($"{job.DataCustom} custom, ", "#ff8c00");
+        WriteLine($"{job.DataToCheck} to check.", "#7fffd4");
+    }
 
-        // Prevent console from closing until the user presses return, then close
+    private static DataPool BuildDataPool(ConsoleOptions opts)
+    {
+        if (string.IsNullOrEmpty(opts.WordlistFile) && !string.IsNullOrEmpty(opts.WordlistRange))
+        {
+            var splitRange = opts.WordlistRange.Split(',');
+            return new RangeDataPool(
+                start: Convert.ToInt64(splitRange[0]),
+                amount: Convert.ToInt32(splitRange[1]),
+                step: splitRange.Length > 2 ? Convert.ToInt32(splitRange[2]) : default,
+                pad: splitRange.Length > 3 && Convert.ToBoolean(splitRange[3]),
+                opts.WordlistType);
+        }
+
+        if (string.IsNullOrEmpty(opts.WordlistFile))
+        {
+            throw new ArgumentException("A wordlist file must be provided when no wordlist range is specified.");
+        }
+
+        return new FileDataPool(opts.WordlistFile, opts.WordlistType);
+    }
+
+    private static Dictionary<string, string> AskCustomInputs(RuriLib.Models.Configs.Config config)
+    {
+        var answers = new Dictionary<string, string>();
+
+        foreach (var input in config.Settings.InputSettings.CustomInputs)
+        {
+            var prompt = new TextPrompt<string>($"{input.Description} ({input.DefaultAnswer}):")
+                .AllowEmpty();
+
+            var answer = AnsiConsole.Prompt(prompt);
+
+            if (!string.IsNullOrWhiteSpace(answer))
+            {
+                input.DefaultAnswer = answer;
+            }
+
+            answers[input.VariableName] = input.DefaultAnswer;
+        }
+
+        return answers;
+    }
+
+    private static void PrintDebuggerLog(object? sender, BotLoggerEntry entry)
+    {
+        if (string.IsNullOrEmpty(entry.Message))
+        {
+            AnsiConsole.WriteLine();
+            return;
+        }
+
+        WriteLine(entry.Message, entry.Color);
+    }
+
+    private static void Write(string text, string? htmlColor = null)
+    {
+        AnsiConsole.Write(new Text(text, new Style(foreground: ToSpectreColor(htmlColor))));
+    }
+
+    private static void WriteLine(string text, string? htmlColor = null)
+    {
+        Write(text, htmlColor);
+        AnsiConsole.WriteLine();
+    }
+
+    private static void WriteStepPromptPanel(RuriLib.Models.Configs.Config config)
+    {
+        var configName = string.IsNullOrWhiteSpace(config.Metadata.Name)
+            ? Path.GetFileNameWithoutExtension(options?.ConfigFile ?? config.Id)
+            : config.Metadata.Name;
+
+        var panelContent = new Markup(
+            $"[deepskyblue1]Step-by-step debugger paused[/]{Environment.NewLine}" +
+            $"[grey]Config:[/] [white]{Markup.Escape(configName)}[/]{Environment.NewLine}" +
+            $"[yellow]Press ENTER to execute the next step[/]{Environment.NewLine}" +
+            $"[grey]Use Ctrl+C to stop the session[/]");
+
+        var panel = new Panel(panelContent)
+        {
+            Header = new PanelHeader("Waiting For Input"),
+            Border = BoxBorder.Rounded,
+            Expand = false
+        };
+
+        panel.BorderStyle = new Style(foreground: Spectre.Console.Color.Aqua);
+        AnsiConsole.Write(panel);
+        AnsiConsole.WriteLine();
+    }
+
+    private static Spectre.Console.Color ToSpectreColor(string? htmlColor)
+    {
+        if (string.IsNullOrWhiteSpace(htmlColor))
+        {
+            return Spectre.Console.Color.Default;
+        }
+
+        var color = htmlColor.Trim().TrimStart('#');
+
+        if (color.Length == 3)
+        {
+            color = string.Concat(color[0], color[0], color[1], color[1], color[2], color[2]);
+        }
+
+        if (color.Length != 6
+            || !byte.TryParse(color.AsSpan(0, 2), System.Globalization.NumberStyles.HexNumber, null, out var r)
+            || !byte.TryParse(color.AsSpan(2, 2), System.Globalization.NumberStyles.HexNumber, null, out var g)
+            || !byte.TryParse(color.AsSpan(4, 2), System.Globalization.NumberStyles.HexNumber, null, out var b))
+        {
+            return Spectre.Console.Color.Default;
+        }
+
+        return new Spectre.Console.Color(r, g, b);
+    }
+
+    private static void WaitForExit()
+    {
+        System.Console.WriteLine("Press ENTER to exit.");
         System.Console.ReadLine();
-        Environment.Exit(0);
     }
 
     private static void PrintResult(object? sender, ResultDetails<MultiRunInput, CheckResult> details)
@@ -200,16 +296,16 @@ Feel free to contribute to the versatility of this project by adding the missing
 
         var color = botData.STATUS switch
         {
-            "SUCCESS" => Color.YellowGreen,
-            "FAIL" => Color.Tomato,
-            "BAN" => Color.Plum,
-            "RETRY" => Color.Yellow,
-            "ERROR" => Color.Red,
-            "NONE" => Color.SkyBlue,
-            _ => Color.Orange
+            "SUCCESS" => "#9acd32",
+            "FAIL" => "#ff6347",
+            "BAN" => "#dda0dd",
+            "RETRY" => "#ffff00",
+            "ERROR" => "#ff0000",
+            "NONE" => "#87ceeb",
+            _ => "#ffa500"
         };
 
-        System.Console.WriteLine($"{botData.STATUS}: {data}", color);
+        WriteLine($"{botData.STATUS}: {data}", color);
     }
 
     private static void PrintTaskError(object? sender, ErrorDetails<MultiRunInput> details)
@@ -221,7 +317,7 @@ Feel free to contribute to the versatility of this project by adding the missing
 
         var proxy = details.Item.BotData.Proxy;
         var data = details.Item.BotData.Line.Data;
-        System.Console.WriteLine($"Task Error: ({proxy})({data})! {details.Exception.Message}", Color.Tomato);
+        WriteLine($"Task Error: ({proxy})({data})! {details.Exception.Message}", "#ff6347");
     }
 
     private static void UpdateTitle()
@@ -242,20 +338,9 @@ Feel free to contribute to the versatility of this project by adding the missing
                                    $"Hits: {job.DataHits} Custom: {job.DataCustom} ToCheck: {job.DataToCheck} Fails: {job.DataFails} Retries: {job.DataRetried + job.DataBanned} | " +
                                    $"Proxies: {job.ProxiesAlive} / {job.ProxiesTotal}";
         }
-        catch (System.InvalidOperationException)
+        catch (InvalidOperationException)
         {
-            /*
-                Unhandled exception. System.InvalidOperationException: Collection was modified; enumeration operation may not execute.
-                at System.Collections.Generic.List`1.Enumerator.MoveNextRare()
-                at System.Linq.Enumerable.Count[TSource](IEnumerable`1 source, Func`2 predicate)
-                at OpenBullet2.Console.Program.UpdateTitle()
-                at OpenBullet2.Console.Program.Run(Options opts)
-                at OpenBullet2.Console.Program.<>c.<Main>b__4_0(Options opts)
-                at CommandLine.ParserResultExtensions.WithParsed[T](ParserResult`1 result, Action`1 action)
-                at OpenBullet2.Console.Program.Main(String[] args)
-             */
-            // The exception above is not easily solvable.
-            // For now we just simply ignore it.
+            // The title reads some collections that might be updated concurrently by the job.
         }
     }
 }
